b97ebeff6b29bdd131df0591c37e2fd1f7823a7a
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon.local>
2021-06-19 17:13:17 +0200

    Create data person type

---
+++ sanity/schemas/person.js
@@ -0,0 +1,39 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  name: 'person',
+  title: 'Slice masters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza name',
+      type: 'string',
+      description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Tell us a bit about this person',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
