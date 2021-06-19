8960fbe7270bd4210093bf2719426ea068a1c9ba
Szymon Świergosz <szymonswiergosz@MacBook-Pro-Szymon.local>
2021-06-19 16:30:22 +0200

    Create topping type and custom preview

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,34 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  name: 'topping',
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping name',
+      type: 'string',
+      description: 'Name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is that vegetarian pizza?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: (fields) => ({
+      title: `${fields.name} ${fields.vegetarian ? '🌱' : ''}`,
+    }),
+  },
+};
